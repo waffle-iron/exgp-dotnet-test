6a0b8c591cd36fc9a6d6ec5223de860700531448
Paul Ewing <pcewing00@gmail.com>
2016-07-29 13:29:13 -0700

    Got protobufs working on .NET core on Ubuntu so that's pretty neat.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -7,6 +8,8 @@ using System.Threading.Tasks;
 
 using ExGP.Messages;
 
+using Google.Protobuf;
+
 namespace exgp_dotnet_test
 {
     public class Program
@@ -14,17 +17,51 @@ namespace exgp_dotnet_test
         public static void Main(string[] args)
         {
             Console.WriteLine("Creating a TCP socket!");
-
             Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
             var ip = IPAddress.Parse("127.0.0.1");
             var port = 10001;
 
+            Console.WriteLine(string.Format("Connecting to {0}:{1}", ip, port));
             socket.Connect(ip, port);
 
-            Envelope envelope = new Envelope();
+            Console.WriteLine("Creating the login request.");
+            var loginRequest = CreateLoginRequest("paul@aol.com", "asdflj4398jfda");
+
+            Console.WriteLine("Encoding the login request.");
+            var encoded = EncodeEnvelope(loginRequest);
+
+            Console.WriteLine("Sending the encoded request.");
+            socket.Send(encoded);
 
             Console.ReadLine();
+
+            socket.Shutdown(SocketShutdown.Both);
+            socket.Dispose();
+        }
+
+        public static Envelope CreateLoginRequest(string email, string passHash)
+        {
+            return new Envelope
+            {
+                AuthPayload = new AuthPayload
+                {
+                    LoginRequest = new LoginRequest
+                    {
+                        Email = email,
+                        PassHash = passHash
+                    }
+                }
+            };
+        }
+
+        public static byte[] EncodeEnvelope(Envelope envelope)
+        {
+            using(MemoryStream stream = new MemoryStream())
+            {
+                envelope.WriteTo(stream);
+                return stream.ToArray();
+            }
         }
     }
 }
